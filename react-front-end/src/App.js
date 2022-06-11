b29dba5f7542c5c523f83c72c5a16c764bdd8c9d
Charles Ngo <charlesngo37@gmail.com>
2022-06-11 10:40:22 -0700

    checking out to master

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import AudioPlayer from './AudioPlayer';
-import { Howl } from 'howler';
+import { Howl, Howler } from 'howler';
 import UserForm from './components/UserForm';
 import Game from './Game'
 
