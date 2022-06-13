b19118018a7e195d9c56df2a067ae46040a44260
Amy Martin <amymariemart@gmail.com>
2022-06-13 16:04:33 -0700

    downloaded dependencies

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 // const axios = require("axios");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8080;
 require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
 
 // helper functions
 const { getToken, getPlaylist } = require("./helpers/spotify");
@@ -17,7 +18,7 @@ const io = socketio(server);
 // global variables
 let token = "";
 let users = [];
-let rooms = []
+let rooms = [];
 
 // express configuration
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,46 +27,42 @@ app.use(express.static("public"));
 
 // retrieves authentication token from spotify
 getToken().then((res) => {
-  token = res.data.access_token
-  console.log(token)
+  token = res.data.access_token;
+  console.log(token);
 });
-
 
 server.listen(PORT, () => {
   console.log(`Express is listening on port ${PORT} 👍`);
 });
 
-
-
 // establishes socket connection
 io.on("connection", (socket) => {
-  const {username, roomId, avatar } = socket.handshake.query;
+  const { username, roomId, avatar } = socket.handshake.query;
   // const roomId = socket.handshake.query.roomId;
   // const avatar = socket.handshake.query.avatar
 
   socket.join(roomId);
-  
-  users.push({id: socket.id, username, roomId, avatar, score: 0});
-  
-  if((index = rooms.findIndex(({Id}) => Id === roomId)) === -1) {
-    rooms.push(
-      {
-        Id: roomId,
-        tracks: [],
-        titles: [],
-        currentTrack: {}
-      }
-    )
+
+  users.push({ id: socket.id, username, roomId, avatar, score: 0 });
+
+  if ((index = rooms.findIndex(({ Id }) => Id === roomId)) === -1) {
+    rooms.push({
+      Id: roomId,
+      tracks: [],
+      titles: [],
+      currentTrack: {},
+    });
   }
   console.log("All Users: ", users);
   console.log("All Rooms: ", rooms);
 
-
-
   //socket.emit("update-users", users.filter((u) => u.roomId === roomId))
 
   socket.on("player-joined", () => {
-    io.in(roomId).emit("update-users", users.filter((u) => u.roomId === roomId))
+    io.in(roomId).emit(
+      "update-users",
+      users.filter((u) => u.roomId === roomId)
+    );
   });
 
   socket.on("Guess", (guess) => {
@@ -73,47 +70,55 @@ io.on("connection", (socket) => {
   });
 
   socket.on("start-game", () => {
-    console.log('game started')
-    io.to(roomId).emit("game-started", `Game has started`); 
+    console.log("game started");
+    io.to(roomId).emit("game-started", `Game has started`);
   });
 
-  socket.on('genre-selected', (genre) => {
+  socket.on("genre-selected", (genre) => {
     getPlaylist(token, genre).then((result) => {
-      const tracks = result.data.tracks
-      const titles = tracks.map( track => track.name)
-      const index = rooms.findIndex(({Id}) => Id === roomId)
-      const updateTracks = {...rooms[index], tracks, titles}
-      rooms[index] = updateTracks
-      console.log('All rooms after update', rooms)
+      const tracks = result.data.tracks;
+      const titles = tracks.map((track) => track.name);
+      const index = rooms.findIndex(({ Id }) => Id === roomId);
+      const updateTracks = { ...rooms[index], tracks, titles };
+      rooms[index] = updateTracks;
+      console.log("All rooms after update", rooms);
     });
-  })
+  });
 
   socket.on("next-round", () => {
-    console.log('next-round')
-    const currentRoom = rooms.findIndex(({Id}) => Id === roomId)
-    const rnmTrackNum = Math.floor(Math.random() * rooms[currentRoom].tracks.length);
+    console.log("next-round");
+    const currentRoom = rooms.findIndex(({ Id }) => Id === roomId);
+    const rnmTrackNum = Math.floor(
+      Math.random() * rooms[currentRoom].tracks.length
+    );
 
-    console.log("The randomest number", rnmTrackNum)
-    console.log("The tracks length before slice", rooms[currentRoom].tracks.length)
+    console.log("The randomest number", rnmTrackNum);
+    console.log(
+      "The tracks length before slice",
+      rooms[currentRoom].tracks.length
+    );
 
-    const newTrack =  rooms[currentRoom].tracks[rnmTrackNum]
-    rooms[currentRoom] = {...rooms[currentRoom], currentTrack: newTrack}
-    rooms[currentRoom].tracks.splice(rnmTrackNum, 1)
+    const newTrack = rooms[currentRoom].tracks[rnmTrackNum];
+    rooms[currentRoom] = { ...rooms[currentRoom], currentTrack: newTrack };
+    rooms[currentRoom].tracks.splice(rnmTrackNum, 1);
 
-    console.log("The tracks length after slice", rooms[currentRoom].tracks.length)
-
+    console.log(
+      "The tracks length after slice",
+      rooms[currentRoom].tracks.length
+    );
 
     io.to(roomId).emit("new-track", rooms[currentRoom].currentTrack);
     //socket.emit("new-track", rooms[currentRoom].currentTrack);
-    console.log(rooms[currentRoom])
+    console.log(rooms[currentRoom]);
   });
- 
-
 
   // disconnects user and removes them from users array
   socket.on("disconnect", () => {
     users = users.filter((u) => u.id !== socket.id);
-    io.in(roomId).emit("update-users", users.filter((u) => u.roomId === roomId))
+    io.in(roomId).emit(
+      "update-users",
+      users.filter((u) => u.roomId === roomId)
+    );
     console.log(`${username} has diconnected!`);
     console.log("All Users: ", users);
   });
--- react-front-end/src/Game.tsx
+++ react-front-end/src/Game.tsx
@@ -1,95 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import { Howl, Howler } from 'howler'
-import { IUser, ISocket } from './interfaces/AppInterfaces';
-import { IGameProps } from './interfaces/GameInterfaces';
-import Leaderboard from './components/LeaderBoard';
-import GameBoard from './components/GameBoard';
-import  AudioPlayer from './components/AudioPlayer'
+import React, { useState, useEffect } from "react";
+import { Howl, Howler } from "howler";
+import { IUser, ISocket } from "./interfaces/AppInterfaces";
+import { IGameProps } from "./interfaces/GameInterfaces";
+import Leaderboard from "./components/LeaderBoard";
+import GameBoard from "./components/GameBoard";
+import AudioPlayer from "./components/AudioPlayer";
 
-const ROUND = 'ROUND'
-const LOBBY = 'LOBBY'
-const COUNTDOWN = 'COUNTDOWN'
+const ROUND = "ROUND";
+const LOBBY = "LOBBY";
+const COUNTDOWN = "COUNTDOWN";
 
 const Game = (props: IGameProps) => {
-  const socket: ISocket = props.socket
+  const socket: ISocket = props.socket;
   const [guess, setGuess] = useState<string>("");
-  const [user, setUser] = useState<IUser>(props.user)
-  const [users, setUsers] = useState<[IUser]>([user])
-  const [track, setTrack] = useState<any>({}) 
-  const [mode, setMode] = useState<string>(LOBBY)
-  const [genre, setGenre] = useState<string>("")
-  
+  const [user, setUser] = useState<IUser>(props.user);
+  const [users, setUsers] = useState<[IUser]>([user]);
+  const [track, setTrack] = useState<any>({});
+  const [mode, setMode] = useState<string>(LOBBY);
+  const [genre, setGenre] = useState<string>("");
+
   useEffect(() => {
-    socket.emit('player-joined', 'hi')
+    socket.emit("player-joined", "hi");
     // return socket.disconnect()
   }, []);
-  
-  useEffect(() => {
-    socket.on('chat-messages', (message: string) => {
-      console.log(message);
-    })
-    socket.on('update-users', (message: [IUser]) => {
-      console.log(message);
-      setUsers(message)
-    })
-    socket.on('game-started', (message: string) => {
-      console.log(message);
-      setMode(COUNTDOWN);    
-      setTimeout(() =>{
-        setMode(ROUND)
-      }, 5000)
-    })
 
-    socket.on('new-track',(message: [any]) => {
+  useEffect(() => {
+    socket.on("chat-messages", (message: string) => {
+      console.log(message);
+    });
+    socket.on("update-users", (message: [IUser]) => {
+      console.log(message);
+      setUsers(message);
+    });
+    socket.on("game-started", (message: string) => {
+      console.log(message);
+      setMode(COUNTDOWN);
+      setTimeout(() => {
+        setMode(ROUND);
+      }, 5000);
+    });
+
+    socket.on("new-track", (message: [any]) => {
       console.log(message);
       setTrack(message);
-    })
-
+    });
   }, [socket]);
 
   const sendGuess = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(`${props.user.username}: ${guess}`)
-    socket.emit('Guess', guess);
-  }
+    console.log(`${props.user.username}: ${guess}`);
+    socket.emit("Guess", guess);
+  };
   const nextRound = () => {
-    setMode(COUNTDOWN); 
-    socket.emit("next-round", 'next-round');
-    setTimeout(() =>{
-      setMode(ROUND)
-    }, 5000)
-  }
+    setMode(COUNTDOWN);
+    socket.emit("next-round", "next-round");
+    setTimeout(() => {
+      setMode(ROUND);
+    }, 5000);
+  };
   const startGame = () => {
-    socket.emit("start-game", 'start');
-    nextRound()
-  }
+    socket.emit("start-game", "start");
+    nextRound();
+  };
 
   const selectGenre = (newGenre: string) => {
-    setGenre(newGenre)
+    setGenre(newGenre);
     socket.emit("genre-selected", newGenre);
-  }
-  
-  return(
+  };
+
+  return (
     <>
       <h2> THE GAME </h2>
-      <GameBoard roomId={props.user.roomId}/>
-      <form onSubmit = {(e) => sendGuess(e)}>
-        <input 
-          type='text'
-          id='guess' 
-          placeholder='Enter guess'
+      <GameBoard roomId={props.user.roomId} />
+      <form onSubmit={(e) => sendGuess(e)}>
+        <input
+          type="text"
+          id="guess"
+          placeholder="Enter guess"
           value={guess}
-          onChange={(e) => setGuess(e.target.value)}/>
-        <button type="submit">
-          Submit
-        </button>
+          onChange={(e) => setGuess(e.target.value)}
+        />
+        <button type="submit">Submit</button>
       </form>
       <button onClick={startGame}>Start</button>
-      <button onClick={() => selectGenre("pop")} >POP</button>
-      <Leaderboard users = {users}/>
+      <button onClick={() => selectGenre("pop")}>POP</button>
+      <Leaderboard users={users} />
       {mode === ROUND && <AudioPlayer src={track.preview_url} />}
-
     </>
-  )
-}
-export default Game
+  );
+};
+
+export default Game;
