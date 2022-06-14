b7b5ae87cc3883dbf5df9f8106619d7ff0f86fbf
Amy Martin <amymariemart@gmail.com>
2022-06-14 16:16:13 -0700

    Styling play game page

--- react-front-end/src/components/GameBoard/PlayGame.tsx
+++ react-front-end/src/components/GameBoard/PlayGame.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { Avatar, Box, Slider } from "@mui/material";
+import { Avatar, Box, Slider, Typography, LinearProgress } from "@mui/material";
+import MinimizeIcon from "@mui/icons-material/Minimize";
 
 import playButton from "../../assets/play-button.png";
 import forward from "../../assets/forward.png";
 import rewind from "../../assets/rewind.png";
 import albumArt from "../../assets/albumArt.png";
+import letterSpace from "../../assets/letterSpace.png";
+import wordSpace from "../../assets/wordSpace.png";
 
 export const PlayGame = () => {
+  const songTitle: string = "Hey Jude";
+  const letterSpaces = [...songTitle].map((char, i) => {
+    if (char !== " ") {
+      return (
+        <MinimizeIcon
+          sx={{
+            width: 50,
+            height: 50,
+          }}
+        ></MinimizeIcon>
+      );
+    }
+    return <img key={i} src={wordSpace} width="30" height="30"></img>;
+  });
+
   return (
     <Box
       sx={{
@@ -14,34 +32,50 @@ export const PlayGame = () => {
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "space-evenly",
+        margin: 2,
       }}
     >
-      <h1>GUESS THE SONG - ROUND 1</h1>
+      <Typography
+        sx={{
+          marginBottom: 2,
+        }}
+        variant="h4"
+        component="h4"
+      >
+        GUESS THE SONG - ROUND 1
+      </Typography>
+
+      <Box
+        component="img"
+        sx={{
+          height: 200,
+          width: 200,
+          border: 3,
+          borderRadius: 2,
+          opacity: 0.75,
+        }}
+        alt="The house from the offer."
+        src={albumArt}
+      />
 
       <Box
         sx={{
           display: "flex",
           flexDirection: "row",
+          spacing: 0,
         }}
       >
-        <img src={albumArt}> </img>
+        {letterSpaces}
       </Box>
 
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-        }}
-      >
-        <Avatar src={rewind}></Avatar>
-        <Avatar src={playButton}></Avatar>
-        <Avatar src={forward}></Avatar>
-      </Box>
+      <LinearProgress variant="determinate" value={70} />
+
       <Slider
         size="small"
         defaultValue={70}
         aria-label="Small"
         valueLabelDisplay="auto"
+        sx={{ width: "40vh" }}
       />
     </Box>
   );
