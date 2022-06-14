9c0b5e564b28bfcb5d3ef8c8446371cc2714399c
Amy Martin <amymariemart@gmail.com>
2022-06-13 20:19:28 -0700

    change util to helpers

---
+++ react-front-end/src/helpers/theme.tsx
@@ -0,0 +1,13 @@
+import { ITheme } from "../Interfaces";
+import { createTheme } from "@mui/material";
+
+export const theme: ITheme = createTheme({
+  palette: {
+    primary: {
+      main: '#3EB489', // Mint Green
+    },
+    secondary: {
+      main: '#3EA4B4', // Pacific Blue
+    },
+  },
+});
