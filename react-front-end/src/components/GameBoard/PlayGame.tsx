d724b31d48ee12a9279bad07beda0c490f194872
Amy Martin <amymariemart@gmail.com>
2022-06-14 17:05:10 -0700

    Add progress bar, and volume icons; fix spacing

--- react-front-end/src/components/GameBoard/PlayGame.tsx
+++ react-front-end/src/components/GameBoard/PlayGame.tsx
@@ -1,15 +1,23 @@
-import React from "react";
-import { Avatar, Box, Slider, Typography, LinearProgress } from "@mui/material";
-import MinimizeIcon from "@mui/icons-material/Minimize";
+import React, { useState } from "react";
 
-import playButton from "../../assets/play-button.png";
-import forward from "../../assets/forward.png";
-import rewind from "../../assets/rewind.png";
+// material UI
+import { Box, Slider, Stack, Typography, LinearProgress } from "@mui/material";
+import MinimizeIcon from "@mui/icons-material/Minimize";
+import VolumeUp from "@mui/icons-material/VolumeUp";
+import VolumeDown from "@mui/icons-material/VolumeDown";
+import VolumeOff from "@mui/icons-material/VolumeOff";
+
 import albumArt from "../../assets/albumArt.png";
-import letterSpace from "../../assets/letterSpace.png";
+
 import wordSpace from "../../assets/wordSpace.png";
 
 export const PlayGame = () => {
+  const [volume, setVolume] = useState([0, 100]);
+
+  // const updateRange = (data: number) => {
+  //   setVal(data);
+  // };
+
   const songTitle: string = "Hey Jude";
   const letterSpaces = [...songTitle].map((char, i) => {
     if (char !== " ") {
@@ -22,6 +30,9 @@ export const PlayGame = () => {
         ></MinimizeIcon>
       );
     }
+
+    const handleChange = () => {};
+
     return <img key={i} src={wordSpace} width="30" height="30"></img>;
   });
 
@@ -32,16 +43,9 @@ export const PlayGame = () => {
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "space-evenly",
-        margin: 2,
       }}
     >
-      <Typography
-        sx={{
-          marginBottom: 2,
-        }}
-        variant="h4"
-        component="h4"
-      >
+      <Typography variant="h4" component="h4" sx={{ marginTop: 2 }}>
         GUESS THE SONG - ROUND 1
       </Typography>
 
@@ -53,30 +57,32 @@ export const PlayGame = () => {
           border: 3,
           borderRadius: 2,
           opacity: 0.75,
+          marginTop: 3,
         }}
         alt="The house from the offer."
         src={albumArt}
       />
 
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          spacing: 0,
-        }}
-      >
+      <Stack spacing={0} direction="row">
         {letterSpaces}
-      </Box>
+      </Stack>
 
-      <LinearProgress variant="determinate" value={70} />
-
-      <Slider
-        size="small"
-        defaultValue={70}
-        aria-label="Small"
-        valueLabelDisplay="auto"
-        sx={{ width: "40vh" }}
+      <LinearProgress
+        variant="determinate"
+        value={50}
+        sx={{ height: 20, width: "45vh", marginTop: 3 }}
       />
+
+      <Stack spacing={2} direction="row" sx={{ marginTop: 3 }}>
+        <VolumeDown />
+        <Slider
+          defaultValue={50}
+          aria-label="Default"
+          valueLabelDisplay="auto"
+          sx={{ width: "20vh" }}
+        />
+        <VolumeUp />
+      </Stack>
     </Box>
   );
 };
