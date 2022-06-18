341dd6c4d09ea159df7283c207234af55ff09b97
Charles Ngo <charlesngo37@gmail.com>
2022-06-18 12:19:37 -0700

    restructed project for heroku

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors"); // allows/disallows cross-site communication
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8080;
+
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8081",
+  "https://songbird-game.herokuapp.com",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+// --> Add this
+app.use(cors(corsOptions));
 
 // helper functions
 const {
@@ -26,6 +48,15 @@ const io = socketio(server);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "react-front-end/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "react-front-end/build", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Express is listening on port ${PORT} 👍`);
